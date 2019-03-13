0f8d382d31593818fe1fa2cb1727e709841c4f22
gravewalker666 <heshan.andris.pro@gmail.com>
2019-03-13 20:11:06 +0530

    edited some files

--- jsonObjectLoader.js
+++ jsonObjectLoader.js
@@ -4,6 +4,8 @@ $(function () {
         url : 'persons.json',
         dataType : 'json',
         success : function (data) {
+            let object = Mustache.render($("#temp").html(),data)
+            $("#content").append(object)
             console.log(data)
         }
     })
